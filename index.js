d7286b76f68d952c9e4e9737be5a43cabb585930
olamide jubril <olamidejubril68@gmail.com>
2023-11-04 09:35:29 +0100

    Simple MQTT Project

---
+++ index.js
@@ -0,0 +1,21 @@
+const mqtt = require("mqtt");
+
+const protocol = 'mqtt'
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `${protocol}://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+client.on('connect', (unknownVar) => {
+  console.log('Connected', { unknownVar })
+})
